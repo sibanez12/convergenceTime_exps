fd1564be26e5c9f70f18f40a3312dec98599057e
Stephen Ibanez <sibanez@g.hmc.edu>
2017-02-13 22:17:27 -0800

    Removed option to set recieve buffer size from tcp server

--- run_server.py
+++ run_server.py
@@ -4,10 +4,9 @@ import socket
 import argparse
 
 
-def run_server(IP, port, bufSize, msgLen):
+def run_server(IP, port, msgLen):
 
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.setsockopt(socket.SOL_SOCKET, socket.SO_RCVBUF, bufSize)
     s.bind((IP, port))
     s.listen(1) # allow one connection
     
@@ -22,7 +21,6 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('IP', type=str, help="the IP address to bind to")
     parser.add_argument('port', type=int, help="the port to start the server on")
-    parser.add_argument('bufSize', type=int, help="the server receive buffer size to use")
     parser.add_argument('msgLen', type=int, help="the max length of the msgs to receive")
     args = parser.parse_args()
 
