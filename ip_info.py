7f0def027c3bb4d3f71073f7f5e5f4390e1af0eb
Stephen Ibanez <sibanez@g.hmc.edu>
2017-09-11 11:13:38 -0700

    Updated ip_info.py

--- ip_info.py
+++ ip_info.py
@@ -1,7 +1,7 @@
 
 
-ip_info = {'10.0.0.2': {'hostname': 'han-2.stanford.edu', 'iface': 'eth2', 'mac': '00:0f:53:0c:71:f8'},
-           '10.0.0.3': {'hostname': 'han-3.stanford.edu', 'iface': 'eth5', 'mac': '00:0f:53:0e:0e:28'},
-           '10.0.0.4': {'hostname': 'han-4.stanford.edu', 'iface': 'eth4', 'mac': '00:0f:53:0e:0c:b0'},
-           '10.0.0.5': {'hostname': 'han-5.stanford.edu', 'iface': 'eth2', 'mac': '00:0f:53:0e:0d:c8'}}
+ip_info = {'10.0.0.1': {'hostname': 'han-1.stanford.edu', 'iface': 'eth2', 'mac': '0c:c4:7a:b7:60:3c'},
+           '10.0.0.3': {'hostname': 'han-3.stanford.edu', 'iface': 'eth7', 'mac': '0c:c4:7a:8e:01:96'},
+           '10.0.0.4': {'hostname': 'han-4.stanford.edu', 'iface': 'eth1', 'mac': '0c:c4:7a:b7:5f:92'},
+           '10.0.0.5': {'hostname': 'han-5.stanford.edu', 'iface': 'eth4', 'mac': '90:e2:ba:4a:d8:84'}}
 
